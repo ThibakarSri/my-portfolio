908aa9eb221a2d65423b000831babf94b351bde6
ThibakarSri <thibakarsri33@gmail.com>
2025-04-18 00:41:30 +0530

    solve errors

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-// import { defineConfig } from "vite"
-// import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/my-portfolio/', // Important!
+  plugins: [react()],
+  base: '/my-portfolio/',
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import ghPages from 'vite-plugin-gh-pages'
+
+// export default defineConfig({
+//   plugins: [react(), ghPages()],
+//   base: '/my-portfolio/', // Important!
+// })
